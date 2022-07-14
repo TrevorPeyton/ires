494536f2b0e4bbea525b4afda2b2b1dd487b3bcf
TrevorPeyton <TrevornPeyton@gmail.com>
2022-07-14 13:03:54 -0400

    Fixed package/module

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setuptools.setup(
     project_urls = {
         "Bug Tracker": "https://github.com/TrevorPeyton/ires/issues"
     },
-    packages=['ires'],
+    py_modules=['ires'],
     install_requires=['requests'],
 )
