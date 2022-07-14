2564da4ed232f430dd6b2ca61161a5eeece7f1e8
TrevorPeyton <TrevornPeyton@gmail.com>
2022-07-14 14:18:11 -0400

    Version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='ires',
-    version='1.0.4',
+    version='1.0.5',
     description='Ionizing Radiation Effects Spectroscopy',
     long_description=long_description,
     long_description_content_type="text/markdown",
