3fe0439143fc4bb8e9062cd431da71253a6c026c
TrevorPeyton <TrevornPeyton@gmail.com>
2022-07-14 14:35:54 -0400

    Fixed interpolation

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='ires',
-    version='1.0.6',
+    version='1.0.7',
     description='Ionizing Radiation Effects Spectroscopy',
     long_description=long_description,
     long_description_content_type="text/markdown",
